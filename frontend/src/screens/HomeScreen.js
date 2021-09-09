f081637291963f4a7ba231642912e0f86f33d5fc
Máté Puskás <puskasmate2000@gmail.com>
2021-09-09 18:15:32 +0200

    Added Rating component to project to see the ratings of each product.

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -9,7 +9,7 @@ const HomeScreen = () => {
             <h1>Legfrissebb termékek</h1>
             <Row>
                 {products.map(product => (
-                    <Col sm={12} lg={6} xl={3}>
+                    <Col key={product._id} sm={12} lg={6} xl={3}>
                         <Product product={product} />
                     </Col>
                 ))}
