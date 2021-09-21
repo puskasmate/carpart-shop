9a4fa8b88fc340a485ce65cec9790cba5e8b0ff3
Máté Puskás <puskasmate2000@gmail.com>
2021-09-21 16:21:54 +0200

    Added custom error handling if a product or a route is not found.

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
-import products from './data/products.js'
+import productRoutes from './routes/productRoutes.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
 dotenv.config()
 
@@ -12,6 +13,12 @@ const app = express()
 app.get('/', (req, res) => {
     res.send('API is running...')
 })
+
+app.use('/api/products', productRoutes)
+
+app.use(notFound)
+
+app.use(errorHandler)
 
 app.get('/api/products', (req, res) => {
     res.json(products)
