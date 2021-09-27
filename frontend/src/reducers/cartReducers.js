f03f21993809285902b23ee33209f1d0fa1d3bad
Máté Puskás <puskasmate2000@gmail.com>
2021-09-27 22:09:39 +0200

    Added remove from cart function to cart screen.

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,22 +1,27 @@
-import { CART_ADD_ITEM } from '../constants/cartConstants.js'
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants/cartConstants.js'
 
 export const cartReducer = (state = { cartItems: [] }, action) => {
     switch(action.type) {
         case CART_ADD_ITEM:
             const item = action.payload
 
-            const existItem = state.cartItems.find(x => x.product === item.product)
+            const existItem = state.cartItems.find((x) => x.product === item.product)
 
             if(existItem) {
                 return {
                     ...state,
-                    cartItems: state.cartItems.map(x => x.product === existItem.product ? item : x)
+                    cartItems: state.cartItems.map((x) => x.product === existItem.product ? item : x)
                 }
             } else {
                 return {
                     ...state,
                     cartItems: [...state.cartItems, item]
                 }
+            }
+        case CART_REMOVE_ITEM:
+            return {
+                ...state,
+                cartItems: state.cartItems.filter((x) => x.product !== action.payload)
             }
         default:
             return state
