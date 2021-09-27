d62726fb20111ce5d3b479687fd8f2bdc56b9b66
Máté Puskás <puskasmate2000@gmail.com>
2021-09-27 17:51:52 +0200

    Implemented add to cart functionality.

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -30,11 +30,5 @@ Rating.defaultProps = {
     color: '#f8e825'
 }
 
-Rating.propTypes = {
-    value: PropTypes.number.isRequired,
-    text: PropTypes.string.isRequired,
-    color: PropTypes.string,
-
-}
 
 export default Rating
