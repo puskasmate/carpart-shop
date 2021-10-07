f8aa4896b969cf740620a9f403d4ac1b821a9567
Máté Puskás <puskasmate2000@gmail.com>
2021-10-07 15:03:13 +0200

    Displaying the user's name in Navbar and imlemented logout functionality.

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,32 +1,49 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { LinkContainer } from 'react-router-bootstrap'
-import { Navbar, Nav, Container } from 'react-bootstrap'
+import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap'
+import { logout } from '../actions/userActions'
 
 const Header = () => {
+    const dispatch = useDispatch()
+
+    const userLogin = useSelector(state => state.userLogin)
+    const { userInfo } = userLogin
+
+    const logoutHandler = () => {
+        dispatch(logout())
+    }
     return (
-<header>
-    <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
-        <Container>
-            <LinkContainer to = '/'>
-            <Navbar.Brand >CarPartShop</Navbar.Brand>    
-            </LinkContainer>
+        <header>
+            <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
+                <Container>
+                    <LinkContainer to = '/'>
+                        <Navbar.Brand >CarPartShop</Navbar.Brand>    
+                    </LinkContainer>
     
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="ms-auto">
-                        <LinkContainer to ='/cart'>
-                            <Nav.Link href="/cart"><i className="fas fa-shopping-cart"></i> Cart</Nav.Link>
-                        </LinkContainer>
-        
-                        <LinkContainer to='login'>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ms-auto">
+                            <LinkContainer to ='/cart'>
+                                <Nav.Link href="/cart"><i className="fas fa-shopping-cart"></i> Cart</Nav.Link>
+                            </LinkContainer>
+                            {userInfo ? (
+                                <NavDropdown title={userInfo.name} id ='username'>
+                                    <LinkContainer to='/profile'>
+                                        <NavDropdown.Item>Profil</NavDropdown.Item>
+                                    </LinkContainer>
+                                    <NavDropdown.Item onClick={logoutHandler}>Kijelentkezés</NavDropdown.Item>
+                                </NavDropdown>
+                            ) : <LinkContainer to='login'>
                             <Nav.Link href="/login"><i className="fas fa-sign-in-alt"></i> Sign in</Nav.Link>
-                        </LinkContainer>
+                        </LinkContainer> }
+                            
         
-                    </Nav>
-                </Navbar.Collapse>
-        </Container>
-    </Navbar>
-</header>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </header>
     )
 }
 
