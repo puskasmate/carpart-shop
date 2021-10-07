89ca75b9f7f6288539ef81f87e19e4d7f61b9ea9
Máté Puskás <puskasmate2000@gmail.com>
2021-10-07 12:16:29 +0200

    User authentication endpoint

---
+++ backend/controllers/userController.js
@@ -0,0 +1,25 @@
+import asyncHandler from 'express-async-handler'
+import User from '../models/userModel.js'
+
+const authUser = asyncHandler(async(req, res) => {
+    const { email, password } = req.body
+
+    const user = await User.findOne({ email })
+
+    if(user && (await user.matchPassword(password))) {
+        res.json({
+            _id:user._id,
+            name: user.name,
+            emial: user.email,
+            isAdmin: user.isAdmin,
+            token: null
+        })
+    } else {
+        res.status(401)
+        throw new Error('Hibás felhasználónév vagy jelszó.')
+    }
+})
+
+export {
+    authUser
+}
