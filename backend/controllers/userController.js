f135501714800627a9b4f90f56a70fe92f05dfc3
Máté Puskás <puskasmate2000@gmail.com>
2021-10-07 15:54:02 +0200

    Implemented update user function in the backend.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -67,8 +67,34 @@ const getUserProfile = asyncHandler(async(req, res) => {
    }
 })
 
+const updateUserProfile = asyncHandler(async(req, res) => {
+    const user = await User.findById(req.user._id)
+ 
+    if (user) {
+         user.name = req.body.name || user.name
+         user.email = req.body.email || user.email
+         if(req.body.password) {
+             user.password = req.body.password
+         }
+
+         const updatedUser = await user.save()
+
+         res.json({
+            _id: updatedUser._id,
+            name: updatedUser.name,
+            emial: updatedUser.email,
+            isAdmin: updatedUser.isAdmin,
+            token: generateToken(updatedUser._id)
+        })
+    } else {
+        res.status(404)
+        throw new Error('Felhasználó nem található!')
+    }
+ })
+
 export {
     authUser,
     getUserProfile,
-    registerUser
+    registerUser,
+    updateUserProfile
 }
