51745d5952b5819105887536e9b839dac355d753
Máté Puskás <puskasmate2000@gmail.com>
2021-10-13 19:01:27 +0200

    Created Place Order Screen to review the order details.

---
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -0,0 +1,103 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { Button, Row, Col, ListGroup, Image, Card, AccordionCollapse } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import Message from '../components/Message'
+import CheckoutSteps from '../components/CheckoutSteps'
+
+const PlaceOrderScreen = () => {
+    const cart = useSelector(state => state.cart)
+
+    cart.itemsPrice = cart.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0)
+
+    cart.shippingPrice = cart.itemsPrice > 150000 ? 0 : 3000
+
+    cart.totalPrice = Number(cart.itemsPrice) + Number(cart.shippingPrice)
+
+    const placeOrderHandler = () => {
+        console.log('Megrendelve!')
+    }
+    return (
+        <>
+            <CheckoutSteps step1 step2 step3 step4 />
+            <Row>
+                <Col md={8}>
+                    <ListGroup variant='flush'>
+                        <ListGroup.Item>
+                            <h2>Szállítás</h2>
+                            <p>
+                                <strong>
+                                    Cím:{' '}
+                                </strong>
+                                {cart.shippingAddress.address}, {cart.shippingAddress.city}{' '}, {cart.shippingAddress.postalCode},{' '} {cart.shippingAddress.country}
+                            </p>
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            <h2>Fizetés{' '}</h2>
+                            <strong>Fizetés módja: </strong>
+                            {cart.paymentMethod}
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            <h2>Rendelni kívánt termékek</h2>
+                            {cart.cartItems.length === 0 ? <Message>A kosár üres!</Message> : (
+                                <ListGroup variant='flush'>
+                                    {cart.cartItems.map((item, index) => (
+                                        <ListGroup.Item key={index}>
+                                            <Row>
+                                                <Col md={1}>
+                                                    <Image src={item.image} alt={item.name} fluid rounded></Image>
+                                                </Col>
+                                                <Col>
+                                                    <Link to={`/product/${item.product}`}>
+                                                        {item.name}
+                                                    </Link>
+                                                </Col>
+                                                <Col md={4}>
+                                                    {item.qty} x {item.price} HUF = {item.qty * item.price} HUF
+                                                </Col>
+                                            </Row>
+                                        </ListGroup.Item>
+                                    ))}
+                                </ListGroup>
+                            )}
+                        </ListGroup.Item>
+                    </ListGroup>
+                </Col>
+                <Col md={4}>
+                    <Card>
+                        <ListGroup variant='flush'>
+                            <ListGroup.Item>
+                                <h2>Rendelés összegzése</h2>
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col>Termékek</Col>
+                                    <Col>{cart.itemsPrice} HUF</Col>
+                                </Row>
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col>Szállítás</Col>
+                                    <Col>{cart.shippingPrice} HUF</Col>
+                                </Row>
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col>Összesen</Col>
+                                    <Col>{cart.totalPrice} HUF</Col>
+                                </Row>
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <Button type='button' className='btn-block w-100' disabled={cart.cartItems === 0} onClick={placeOrderHandler}>
+                                    MEGRENDELEM
+                                </Button>
+                            </ListGroup.Item>
+                        </ListGroup>
+                    </Card>
+                </Col>
+            </Row>
+        </>
+    )
+}
+
+export default PlaceOrderScreen
