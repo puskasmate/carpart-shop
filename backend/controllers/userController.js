be4530929c7b00f4db42966582834ea6cbd62e04
Máté Puskás <puskasmate2000@gmail.com>
2021-10-20 15:25:19 +0200

    Created get user by id and update user endpoints only for admins.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -111,6 +111,38 @@ const updateUserProfile = asyncHandler(async(req, res) => {
     }
  })
  
+ const getUserById = asyncHandler(async(req, res) => {
+    const user = await User.findById(req.params.id).select('-password')
+    if(user) {
+        res.json(user)
+    } else {
+        res.status(404)
+        throw new Error('Felhasználó nem található!')
+    }
+ })
+
+ const updateUser = asyncHandler(async(req, res) => {
+     
+    const user = await User.findById(req.params.id)
+ 
+    if (user) {
+         user.name = req.body.name || user.name
+         user.email = req.body.email || user.email
+         user.isAdmin = req.body.email
+
+         const updatedUser = await user.save()
+
+         res.json({
+            _id: updatedUser._id,
+            name: updatedUser.name,
+            email: updatedUser.email,
+            isAdmin: updatedUser.isAdmin,
+        })
+    } else {
+        res.status(404)
+        throw new Error('Felhasználó nem található!')
+    }
+ })
 
 export {
     authUser,
@@ -118,5 +150,7 @@ export {
     registerUser,
     updateUserProfile,
     getUsers,
-    deleteUser
+    deleteUser,
+    getUserById,
+    updateUser
 }
