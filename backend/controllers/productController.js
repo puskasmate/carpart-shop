2200abd17391ab54af820579150d3be418d1d071
Máté Puskás <puskasmate2000@gmail.com>
2021-10-21 15:03:35 +0200

    Created create and update product endpoint and filled products with sample data.

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -31,8 +31,57 @@ const deleteProduct = asyncHandler(async(req, res) => {
     }
 })
 
+const createProduct = asyncHandler(async(req, res) => {
+    const product = new Product({
+        name: 'Példa név',
+        price: 0,
+        user: req.user._id,
+        image: '/images/pelda.jpg',
+        brand: 'Példa márka',
+        category: 'Példa kategória',
+        subcategory: 'Példa alkategória',
+        model: 'Példa model',
+        year: 2001,
+        countInStock: 0,
+        numReviews: 0,
+        description: 'Példa leírás'
+    })
+
+    const createdProduct = await product.save()
+    res.status(201).json(createdProduct)
+})
+
+const updateProduct = asyncHandler(async(req, res) => {
+    const { name, price, description, image, brand, category, subcategory, model, countInStock, year } = req.body
+    
+    const product = await Product.findById(req.params.id)
+
+    if(product) {
+        product.name = name
+        product.price = price
+        product.description = description
+        product.image = image
+        product.category = category
+        product.subcategory = subcategory
+        product.brand = brand
+        product.model = model
+        product.countInStock = countInStock
+        product.year = year
+
+        const updatedProduct = await product.save()
+        res.json(updatedProduct)
+    } else {
+        res.status(404)
+        throw new Error('Termék nem található!')
+    }
+
+    
+})
+
 export {
     getProducts,
     getProductById,
-    deleteProduct
+    deleteProduct,
+    createProduct,
+    updateProduct
 }
