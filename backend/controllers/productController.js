fa265b425ba6d65dfd8d09f024f51f1132e34ae0
Máté Puskás <puskasmate2000@gmail.com>
2021-10-22 15:07:07 +0200

    Created create review endpoint and added morgan to log all the http requests.

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -78,10 +78,47 @@ const updateProduct = asyncHandler(async(req, res) => {
     
 })
 
+const createReview = asyncHandler(async(req, res) => {
+    const { rating, comment } = req.body
+    
+    const product = await Product.findById(req.params.id)
+
+    if(product) {
+        const alreadyReviewed = product.reviews.find(review => review.user.toString() === req.user._id.toString())
+
+        if(alreadyReviewed) {
+            res.status(400)
+            throw new Error('A termékhez már alkotott véleményt!')
+        }
+
+        const review = {
+            name: req.user.name,
+            rating: Number(rating),
+            comment,
+            user: req.user._id
+        }
+
+        product.reviews.push(review)
+
+        product.numReviews = product.reviews.length
+
+        product.rating = product.reviews.reduce((acc, item) => item.rating + acc, 0) / product.reviews.length
+        
+        await product.save()
+        res.status(201).json({ message: 'Vélemény megalkotva! Köszönjük visszajelzését!'})
+    } else {
+        res.status(404)
+        throw new Error('Termék nem található!')
+    }
+
+    
+})
+
 export {
     getProducts,
     getProductById,
     deleteProduct,
     createProduct,
-    updateProduct
+    updateProduct,
+    createReview
 }
